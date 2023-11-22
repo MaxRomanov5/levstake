9403e01835c482d4d3a5c8e144a0639bbf6a9ba0
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2023-11-22 18:22:11 +0200

    context

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles';
-import { UserContext } from "path/to/userContext.js";
+
 import theme from './theme/index.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserContext.Provider value={{ username: "Mango" }}>
+
 
     <ThemeProvider theme={theme}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
     </ThemeProvider>
-    </UserContext.Provider>
+  
   </React.StrictMode>
 )
