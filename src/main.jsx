0ba86f452dd7275cfd8ff1fb6b6ac7cf9545e14f
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2023-11-30 20:37:59 +0200

    web3

--- src/main.jsx
+++ src/main.jsx
@@ -15,12 +15,19 @@ import { MetaMaskProvider } from '@metamask/sdk-react';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <MetaMaskProvider debug={false} sdkOptions={{
-      checkInstallationImmediately: false,
-      dappMetadata: {
-        name: "Demo React App",
-        url: window.location.host,
-      }
+ <MetaMaskProvider debug={false} sdkOptions={{
+      logging:{
+          developerMode: false,
+        },
+       
+        checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
+        i18nOptions: {
+          enabled: true,
+        },
+        dappMetadata: {
+          name: "Demo React App",
+          url: window.location.protocol + '//' + window.location.host,
+        }
     }}>
 <UserProvider>
     <ThemeProvider theme={theme}>
--- src/pages/ConnectWallet/ConnectWallet.jsx
+++ src/pages/ConnectWallet/ConnectWallet.jsx
@@ -26,22 +26,14 @@ const ConnectWallet = () => {
 
 const [account, setAccount] = useState('');
   const { sdk, connected, connecting, provider, chainId } = useSDK();
-  console.log(sdk);
-  if(sdk){
-    sdk.connect()
-  }
 
-//   const connect = async () => {
-//     try {
-//       const accounts = await sdk.connect();
-//       console.log(accounts);
-//       setAccount(accounts?.[0]);
-//     } catch(err) {
-//       console.warn(`failed to connect..`, err);
-//     }
-//   };
-//   connect()
-// console.log(sdk);
+  const connect = async () => {
+    try {
+      await sdk?.connect();
+    } catch (err) {
+      console.warn(`failed to connect..`, err);
+    }
+  };
 
 
 // const signMessage = useSignMessage({
@@ -60,7 +52,7 @@ async function mainConnecting (){
 
 //   try {
 
-//       // await open()
+      await open()
 
 //     const wallet = await api.metaMaskConnecting() 
 
@@ -106,7 +98,7 @@ async function mainConnecting (){
             <Typography color='primary.main' variant='h2' sx={{fontSize:'32px',lineHeight:'35px',marginBottom:'32px'}}>Connect wallet to start</Typography>
             <Stack sx={{backgroundColor:'white',padding:'32px 24px',borderRadius:'8px',gap:'16px'}} justifyContent='space-between' alignItems='center' >
 <img style={{width:'152px',height:'28px'}}  src={images.metamask} alt="metamask" />
-            <Button onClick={()=>mainConnecting()} variant="contained" sx={{backgroundColor:'purpleBG.main',padding:"12px 16px",borderRadius:'8px',display:'flex',border:'2px solid white',borderWidth:'0px','&.MuiButtonBase-root.MuiButton-root:hover':{backgroundColor:'#7A56F8'}}}><img style={{marginRight:'8px'}} src={images.hdmi} alt="hdmi"></img><Typography variant="subtitle1" sx={{fontSize:'14px',lineHeight:'16.24px',color:'primary.main',fontWeight:'500'}} > Connect wallet</Typography></Button>
+            <Button onClick={connect} variant="contained" sx={{backgroundColor:'purpleBG.main',padding:"12px 16px",borderRadius:'8px',display:'flex',border:'2px solid white',borderWidth:'0px','&.MuiButtonBase-root.MuiButton-root:hover':{backgroundColor:'#7A56F8'}}}><img style={{marginRight:'8px'}} src={images.hdmi} alt="hdmi"></img><Typography variant="subtitle1" sx={{fontSize:'14px',lineHeight:'16.24px',color:'primary.main',fontWeight:'500'}} > Connect wallet</Typography></Button>
             </Stack>
         </div>
         {/* {<ModalConnect></ModalConnect>} */}
