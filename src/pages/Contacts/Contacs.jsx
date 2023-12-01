7e2d781360f261054a334fe3e66689b3baf5c302
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2023-12-02 00:33:56 +0200

    walletconnect

--- src/pages/Contacts/Contacs.jsx
+++ src/pages/Contacts/Contacs.jsx
@@ -11,7 +11,7 @@ const { isLoggedIn, username, logIn, logOut } = useUser()
 
     return (
         <div style={{paddingTop:'94px',maxWidth:'600px',margin:'0 auto'}}>
-
+            <button onClick={()=>{logOut()}}>logout</button>
    <Typography sx={{marginBottom:'24px',textAlign:'center',...(desk&&{fontSize:'64px',lineHeight:'70px'})}} color='primary.main' variant="h2">Need help?</Typography> 
            <Typography sx={{marginBottom:'80px',textAlign:'center'}} color='primary.main' variant="subtitle1" >Ut vestibulum, tortor faucibus eu. Lacus, neque commodo ipsum amet. Consectetur vitae faucibus facilisi nulla sollicitudin malesuada tristique. Venenatis libero, pellentesque pharetra molestie in bibendum.</Typography>
    <FeedbackForm></FeedbackForm>
