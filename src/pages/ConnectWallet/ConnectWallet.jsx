0382c6e0becd66b28d503d5674d5fa80ad5da36f
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2023-12-01 12:21:39 +0200

    web13

--- src/pages/ConnectWallet/ConnectWallet.jsx
+++ src/pages/ConnectWallet/ConnectWallet.jsx
@@ -1,6 +1,6 @@
 import images from '../../assets/images';
 import styled from './ConnectWallet.module.css'
-import {useState,useEffect} from 'react'
+import {useState, useRef} from 'react'
 import {Typography,Stack,Button,useMediaQuery,useTheme} from '@mui/material'
 import api from '../../API/levstake.js'
 import { useNavigate } from "react-router-dom";
@@ -14,13 +14,13 @@ import { useSDK } from '@metamask/sdk-react';
 
 const ConnectWallet = () => {
 
-
+useRef
   const theme= useTheme()
   const mob = useMediaQuery(theme.breakpoints.down('md'))
   const navigate = useNavigate();
   const {logIn,isLoggedIn} = useUser()
 
-  
+  const click = useRef(0);
 
 // const { address, isConnecting, isDisconnected } = useAccount()
 
@@ -31,9 +31,10 @@ const [account, setAccount] = useState('');
 // console.log(sdk);
   const connectAndSign = async () => {
     try {
-
+      
+      console.log(click);
       await sdk.connect()
-
+      click.current=1
       const authString = await api.getAuthData()
       console.log(authString);
       const signResult = await sdk.connectAndSign({
@@ -126,10 +127,10 @@ async function mainConnecting (){
          
         <div className={styled.box}>
         <div>{account}</div>
-            <Typography color='primary.main' variant='h2' sx={{fontSize:'32px',lineHeight:'35px',marginBottom:'32px'}}>Connect wallet to start</Typography>
+            <Typography color='primary.main' variant='h2' sx={{fontSize:'32px',lineHeight:'35px',marginBottom:'32px'}}>{click.current ===0 ?'Connect wallet to start' : 'Please Sign'}</Typography>
             <Stack sx={{backgroundColor:'white',padding:'32px 24px',borderRadius:'8px',gap:'16px'}} justifyContent='space-between' alignItems='center' >
 <img style={{width:'152px',height:'28px'}}  src={images.metamask} alt="metamask" />
-            <Button onClick={connectAndSign} variant="contained" sx={{backgroundColor:'purpleBG.main',padding:"12px 16px",borderRadius:'8px',display:'flex',border:'2px solid white',borderWidth:'0px','&.MuiButtonBase-root.MuiButton-root:hover':{backgroundColor:'#7A56F8'}}}><img style={{marginRight:'8px'}} src={images.hdmi} alt="hdmi"></img><Typography variant="subtitle1" sx={{fontSize:'14px',lineHeight:'16.24px',color:'primary.main',fontWeight:'500'}} > Connect wallet</Typography></Button>
+            <Button onClick={connectAndSign} variant="contained" sx={{backgroundColor:'purpleBG.main',padding:"12px 16px",borderRadius:'8px',display:'flex',border:'2px solid white',borderWidth:'0px','&.MuiButtonBase-root.MuiButton-root:hover':{backgroundColor:'#7A56F8'}}}><img style={{marginRight:'8px'}} src={images.hdmi} alt="hdmi"></img><Typography variant="subtitle1" sx={{fontSize:'14px',lineHeight:'16.24px',color:'primary.main',fontWeight:'500'}} > {click.current ===0 ?'Connect wallet':'Sign'}</Typography></Button>
             </Stack>
         </div>
         {/* {<ModalConnect></ModalConnect>} */}
